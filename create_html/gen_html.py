2a3f18c792c13678d63244630c0848b0e5a4d7ed
Wei Huang <Wei.Huang@noaa.gov>
2021-02-26 15:20:06 -0600

    add gen_html.py

--- create_html/gen_html.py
+++ create_html/gen_html.py
@@ -32,54 +32,158 @@ class GenHTML4JEDI:
       print('self.htmldir: ', self.htmldir)
 
   def process(self):
-   #onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    self.filelist = {}
 
     filelist = []
     dir_list = []
     for item in listdir(self.workdir):
-      if(isfile(join(self.workdir, item))):
-        filelist.append(item)
+      name = join(self.workdir, item)
+      if(isfile(name)):
+        filelist.append(name)
       else:
-        dir_list.append(item)
+        dir_list.append(name)
 
-    if(self.debug):
-      print('filelist: ', filelist)
-      print('dir_list: ', dir_list)
+   #if(self.debug):
+   #  print('filelist: ', filelist)
+   #  print('dir_list: ', dir_list)
 
-    parentdir = self.workdir
-    for newdir in dir_list:
-      self.process_dir(parentdir, newdir)
+    for d in dir_list:
+      if(d.find('.git') > 0):
+        continue
+      self.process_dir(d)
+     #break
 
-  def process_dir(self, parentdir, newdir):
-    currentdir = join(parentdir, newdir)
-    parentdir = currentdir
+   #for f in filelist:
+   #  self.process_file(self.workdir, self.htmldir, f)
+
+  def process_dir(self, d):
     filelist = []
     dir_list = []
-    for item in listdir(currentdir):
-      if(isfile(join(currentdir, item))):
-        filelist.append(item)
+    for item in listdir(d):
+      name = join(d, item)
+      if(isfile(name)):
+        filelist.append(name)
       else:
-        dir_list.append(item)
+        dir_list.append(name)
 
-    if(self.debug):
-      print('filelist: ', filelist)
-      print('dir_list: ', dir_list)
+   #if(self.debug):
+   #  print('filelist: ', filelist)
+   #  print('dir_list: ', dir_list)
 
-    parentdir = currentdir
-    for newdir in dir_list:
-      self.process_dir(parentdir, newdir)
+    for d in dir_list:
+      if(d.find('.git') > 0):
+        continue
+      self.process_dir(d)
 
-  def stats(self, flnm):
+    for f in filelist:
+      self.preprocess_file(f)
+
+  def iscode(self, flnm):
+    item = flnm.split('.')
+    if(len(item) > 1):
+      if(item[-1] in ['h', 'c', 'cc', 'cxx', 'f', 'F', 'f90', 'F90']):
+        return 1
+    return 0
+
+  def preprocess_file(self, flnm):
+   #if(os.path.exists(flnm)):
+   #  if(self.debug):
+   #    print('Process file: ', flnm)
+   #else:
+   #  print('Filename ' + flnm + ' does not exit. Stop')
+   #  sys.exit(-1)
+
+    htmlfile = flnm.replace(self.workdir, self.htmldir)
+    htmlfile = '%s.html' %(htmlfile)
+    htmldir, filename = os.path.split(htmlfile)
+    os.system('mkdir -p ' + htmldir)
+
+    srccode = self.iscode(flnm)
+
+    if(srccode):
+      sp = len(self.workdir)
+      hp = len(self.htmldir)
+      sname = flnm[sp+1:]
+      self.filelist[sname] = {}
+      self.filelist[sname]
+      self.filelist[sname]['html'] = htmlfile[hp:]
+
+      with open(flnm, 'r', encoding="ascii", errors="surrogateescape") as fp:
+        print('Process file: ', sname)
+       #print('Process file: ', htmlfile[hp+1:])
+        lines = fp.readlines()
+       #num_lines = len(lines)
+       #print('Total number of lines: ', num_lines)
+        for line in lines:
+          if(line.find('#include') >= 0):
+            if(line.find('>') < 0):
+              print(line.rstrip())
+   #else:
+   #  self.print_file(flnm)
+
+  def isdatafile(self, flnm):
+    item = flnm.split('.')
+    if(len(item) > 1):
+      if(item[-1] in ['nc', 'nc4', 'hdf', 'h5', 'pdf', 'png', 'gif', 'jpeg']):
+        return 1
+    return 0
+
+  def print_file(self, flnm):
     if(os.path.exists(flnm)):
-      pass
+      if(self.isdatafile(flnm)):
+        return
+      else:
+        print('Print file: ', flnm)
     else:
       print('Filename ' + flnm + ' does not exit. Stop')
       sys.exit(-1)
 
+    htmlfile = flnm.replace(self.workdir, self.htmldir)
+    htmlfile = '%s.html' %(htmlfile)
+    htmldir, filename = os.path.split(htmlfile)
+    os.system('mkdir -p ' + htmldir)
+
+    sp = len(self.workdir)
+    HTML = open(htmlfile, 'w')
+    HTML.write('<h2>' + flnm[sp:] + '</h2>\n')
+    HTML.write('<html>\n')
+    HTML.write('<body>\n')
+    HTML.write('<ol>\n')
+
     with open(flnm) as fp:
       lines = fp.readlines()
-      num_lines = len(lines)
-      print('Total number of lines: ', num_lines)
+     #num_lines = len(lines)
+     #print('Total number of lines: ', num_lines)
+      for line in lines:
+        HTML.write('  <li>' + line.rstrip() + '</li>\n')
+
+    HTML.write('</ol>\n')
+    HTML.write('</body>\n')
+    HTML.write('</html>\n')
+    HTML.close()
+    
+"""
+<h1>Largest Heading</h1>
+<h2> . . . </h2>
+<h3> . . . </h3>
+<h4> . . . </h4>
+<h5> . . . </h5>
+<h6>Smallest Heading</h6>
+
+<p>This is a paragraph.</p>
+<br> (line break)
+<hr> (horizontal rule)
+<!-- This is a comment -->
+
+<b>Bold text</b>
+<code>Computer code</code>
+<em>Emphasized text</em>
+<i>Italic text</i>
+<kbd>Keyboard input</kbd> 
+<pre>Preformatted text</pre>
+<small>Smaller text</small>
+<strong>Important text</strong>
+"""
 
 #--------------------------------------------------------------------------------
 if __name__== '__main__':
---
+++ vis_tools/plotact.py
@@ -0,0 +1,318 @@
+#########################################################################
+#$Id: bld.py 28 2021-01-21 15:10:31Z whuang $
+#$Revision: 28 $
+#$HeadURL: file:///Users/whuang/.wei_svn_repository/trunk/jedi-build-tools/bld.py $
+#$Date: 2021-01-21 08:10:31 -0700 (Thu, 21 Jan 2021) $
+#$Author: whuang $
+#########################################################################
+
+import getopt
+import os, sys
+import subprocess
+import time
+import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def cmdout(command):
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+    ostr = result.stdout
+    return ostr.strip()
+
+""" Profiler """
+class Profiler:
+  """ Constructor """
+  def __init__(self, debug=0, threadlist=[], corelist=[], enslist=[],
+               workdir=None, linear=0, show=0):
+
+    """ Initialize class attributes """
+    self.debug = debug
+    self.workdir = workdir
+    self.threadlist = threadlist
+    self.corelist = corelist
+    self.enslist = enslist
+    self.linear = linear
+    self.show = show
+
+    if(workdir is None):
+      print('workdir not defined. Exit.')
+      sys.exit(-1)
+
+    self.color_list = ['cyan', 'blue', 'red', 'darkgreen', 'darkblue']
+    self.function_list = ['applyWeights', 'computeWeights', 'total']
+    self.fullfunction_list = ['oops::GETKFSolver::applyWeights', 'oops::GETKFSolver::computeWeights',
+                              'util::Timers::Total']
+
+  def process(self):
+    self.filelist = {}
+    self.stats_list = {}
+    for core in self.corelist:
+      self.filelist[core] = {}
+      self.stats_list[core] = {}
+      for member in self.enslist:
+        self.filelist[core][member] = {}
+        self.stats_list[core][member] = {}
+        self.stats_list[core][member][0] = []
+        self.stats_list[core][member][1] = []
+        self.stats_list[core][member][2] = {}
+        for n in self.threadlist:
+          ncore = core / n
+          flnm = '%s/omp%d/n%dm%do%d/log.run.np%d_nens%d' %(self.workdir, n, ncore, member, n, ncore, member)
+
+          if(os.path.exists(flnm)):
+            print('Processing file: ', flnm)
+            avgtime, ttltime = self.stats(flnm)
+            if((ttltime > 0.0) and (len(avgtime) == len(self.function_list))):
+              self.stats_list[core][member][0].append(n)
+              self.stats_list[core][member][1].append(ttltime)
+              self.stats_list[core][member][2][n] = avgtime
+          else:
+            pinfo = 'Log file %s/omp%d/n%dm%do%d/log.run.np%d_nens%d does not exist.' %(self.workdir, n, ncore, member, n, ncore, member)
+            print(pinfo)
+        nt = len(self.stats_list[core][member][0])
+        pinfo = 'Process %d core, %d member has cases: %d' %(core, member, nt)
+        print(pinfo)
+
+  def stats(self, flnm):
+    ttltime = -1.0
+    avgtime = []
+
+    with open(flnm) as fp:
+      lines = fp.readlines()
+     #line = fp.readline()
+      num_lines = len(lines)
+     #print('Total number of lines: ', num_lines)
+
+      nl = 0
+      while(nl < num_lines):
+        if(lines[nl].find('Parallel Timing Statistics') > 0):
+         #if(self.debug):
+         #  print('Start Parallel Timing Statistics')
+          nl, avgtime, ttltime = self.parallel_time_stats(lines, nl)
+          nl += num_lines
+        nl += 1
+
+    return avgtime, ttltime
+
+  def parallel_time_stats(self, lines, nl):
+    avgtime = []
+    ttltime = -1.0
+    going = 1
+    ns = nl + 3
+    while(going):
+      line = lines[ns].strip()
+      ns += 1
+      if(line.find('Parallel Timing Statistics') > 0):
+        going = 0
+        break
+
+     #print('Line ' + str(ns) + ': ' + line)
+
+      item = line.split(' : ')
+     #print(item)
+      nlist = item[0].strip().split(' ')
+      name = nlist[1]
+
+      tstr = item[1].strip()
+      while(tstr.find('  ') > 0):
+        tstr = tstr.replace('  ', ' ')
+      nlist = tstr.split(' ')
+
+      for funcname in self.fullfunction_list:
+        if(name == funcname):
+          avgtime.append(float(nlist[2]))
+         #print('      ' + name + ':' + nlist[2])
+
+      if(name == 'util::Timers::Total'):
+        ttltime = float(nlist[2])
+       #print('      Total time:', ttltime)
+
+    return ns, avgtime, ttltime
+
+  def plot(self):
+    self.rows, self.cols = len(self.enslist), len(self.corelist)
+
+   #fig, axes = plt.subplots(self.rows, self.cols, sharex='col', sharey='row')
+   #fig, axes = plt.subplots(self.rows, self.cols, sharex='col', sharey='row', figsize=(10,10))
+    fig, axes = plt.subplots(self.rows, self.cols, figsize=(10,10))
+
+   #fig.suptitle('This is a somewhat long figure title', fontsize=16)
+    fig.suptitle(self.workdir, fontsize=12)
+
+   #add a big axis, hide frame
+   #fig.add_subplot(111, frameon=False)
+   #hide tick and tick label of the big axis
+   #plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+   #plt.xlabel('Threads')
+   #plt.ylabel('Time (sencond)')
+
+    if(self.linear):
+      imgname = 'lin_act.png'
+    else:
+      imgname = 'log_act.png'
+
+    self.function_timing = {}
+    for nc in range(len(self.corelist)):
+      core = self.corelist[nc]
+      self.function_timing[core] = {}
+      for nm in range(len(self.enslist)):
+        member = self.enslist[nm]
+        nt = len(self.stats_list[core][member][0])
+        pinfo = 'Plot %d core, %d member has cases: %d' %(core, member, nt)
+        print(pinfo)
+
+        if(nt > 1):
+          self.plot_act(axes[nm, nc], core, member)
+
+   #Create the legend
+    fig.legend(axes,
+               labels=self.function_list,   # The labels for each line
+               loc="center right",   # Position of legend
+               borderaxespad=0.1,    # Small spacing around legend box
+               title="Function Colors"  # Title for the legend
+               )
+
+   #plt.subplots_adjust(right=0.85)
+    fig.tight_layout(rect=(0,0,1,0.8))
+
+   #plt.tight_layout()
+
+    if(self.show):
+      plt.show()
+    else:
+      fig.savefig(imgname)
+
+    plt.cla()
+    plt.clf()
+
+  def plot_act(self, ax, core, member):
+    title = '%d cores, %d members' %(core, member)
+
+    threads = self.stats_list[core][member][0]
+   #print('core: ', core)
+   #print('member: ', member)
+   #print('Threads: ', threads)
+   #print('self.stats_list[core][member][2]: ', self.stats_list[core][member][2])
+
+    x = np.array(threads)
+    ts = {}
+    line = []
+
+    ymin = 1.0e+36
+    ymax = 0.0
+
+    nf = len(self.function_list)
+    for n in range(nf):
+      pl = None
+      line.append(pl)
+      ts[n] = []
+      for i in threads:
+        t = 0.001*self.stats_list[core][member][2][i][n]
+        ts[n].append(t)
+        if(t > ymax):
+          ymax = t
+        if(t < ymin):
+          ymin = t
+
+    if(self.linear):
+      tmin = 10.0*int(ymin/10.0) - 10.0
+      tmax = 10.0 + 10.0*int(ymax/10.0)
+    else:
+      tmin = 8192.0
+      while(tmin > ymin):
+        tmin /= 2.0
+      tmax = 1.0
+      while(tmax < ymax):
+        tmax *= 2.0
+
+    nt = len(self.threadlist)
+   #print('self.threadlist = ', self.threadlist)
+    ax.set_xlim([self.threadlist[0], self.threadlist[nt-1]])
+
+    nt = len(self.threadlist)
+    ax.set_ylim([tmin, tmax])
+
+   #multiple line plot
+    for num in range(nf):
+      line[num] = ax.plot(x, ts[num], marker='', color=self.color_list[num],
+                          linewidth=2, alpha=0.9, label=self.function_list[num])[0]
+
+   #Add title and axis names
+    ax.set_title(title)
+
+    ax.set_xlabel('Threads')
+    ax.set_ylabel('Time(s)')
+
+    ax.set_xscale("log", base=2)
+
+    if(self.linear):
+      pass
+    else:
+     #ax.set_yscale("log", base=2)
+      ax.set_yscale("log", base=10)
+
+   #Show the major grid lines with dark grey lines
+    ax.grid(b=True, which='major', color='#666666', linestyle='dotted')
+
+   #Show the minor grid lines with very faint and almost transparent grey lines
+    ax.minorticks_on()
+    ax.grid(b=True, which='minor', color='#999999', linestyle='dotted', alpha=0.2)
+
+  def set_linear(self, linear=1):
+    self.linear = linear
+
+  def set_show(self, show=1):
+    self.show = show
+
+#--------------------------------------------------------------------------------
+if __name__== '__main__':
+  debug = 1
+ #workdir = '/work/noaa/gsienkf/weihuang/jedi/base'
+ #workdir = '/work/noaa/gsienkf/weihuang/jedi/case1'
+ #workdir = '/work/noaa/gsienkf/weihuang/jedi/case2'
+ #workdir = '/work/noaa/gsienkf/weihuang/jedi/case3'
+ #workdir = '/work/noaa/gsienkf/weihuang/jedi/case4'
+  workdir = '/work/noaa/gsienkf/weihuang/jedi/intelbase'
+ #workdir = '/work/noaa/gsienkf/weihuang/jedi/intelcase'
+ #workdir = '/work/noaa/gsienkf/weihuang/jedi/intelcase1'
+ #workdir = '/work/noaa/gsienkf/weihuang/jedi/intelcase0'
+  enslist = [10, 20, 40, 80]
+ #enslist = [10, 20, 40]
+  threadlist = [1, 2, 4, 8]
+  linear = 1
+  corelist = [24, 240]
+  show = 1
+
+  opts, args = getopt.getopt(sys.argv[1:], '', ['debug=', 'workdir=',
+    'corelist=', 'enslist=', 'threadlist=', 'ncore='])
+
+  for o, a in opts:
+    if o in ('--debug'):
+      debug = int(a)
+    elif o in ('--workdir'):
+      workdir = a
+    elif o in ('--corelist'):
+      corelist = a
+    elif o in ('--enslist'):
+      enslist = a
+    elif o in ('--threadlist'):
+      threadlist = a
+    elif o in ('--linear'):
+      linear = int(a)
+    elif o in ('--show'):
+      show = int(a)
+    else:
+      assert False, 'unhandled option'
+
+  pr = Profiler(debug=debug, threadlist=threadlist, corelist=corelist,
+                enslist=enslist, workdir=workdir, show=show)
+  pr.process()
+
+  for show in [1, 0]:
+ #for show in [0]:
+    pr.set_show(show=show)
+    for linear in [0, 1]:
+      pr.set_linear(linear=linear)
+      pr.plot()
+
