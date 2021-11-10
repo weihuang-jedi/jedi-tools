792aa4b4b4b5e49c6c7d13d49ebab0b93f7432c4
Wei Huang <wei.huang@noaa.gov>
2021-11-09 18:30:40 -0600

    and a new script

---
+++ regrid/ensinterp.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+#SBATCH --ntasks-per-node=6
+#SBATCH -N 1
+#SBATCH -n 6
+#SBATCH -t 01:15:00
+#SBATCH -A gsienkf
+##SBATCH --partition=orion
+#SBATCH --partition=bigmem
+#SBATCH --job-name=interp
+#SBATCH --output=log.interp.o%j
+##SBATCH --mem=0
+
+ set -x
+
+ module load esmf
+
+ ulimit -s unlimited
+ ulimit -c unlimited
+
+ generate_weights=0
+
+ executable=/work2/noaa/gsienkf/weihuang/tools/weiinterp/fv3interp2latlon.exe
+
+ ensdir=/work/noaa/gsienkf/weihuang/jedi/EnsembleDaVal/Data/ens
+ latlondir=/work/noaa/gsienkf/weihuang/jedi/src/my.compile/saber/test/latlondata
+
+ number_members=80
+
+ n=1
+ while [ $n -le $number_members ]
+ do
+   if [ $n -lt 10 ]
+   then
+     member_str=mem00${n}
+   elif [ $n -lt 100 ]
+   then
+     member_str=mem0${n}
+   else
+     member_str=mem${n}
+   fi
+
+   datadir=${ensdir}/${member_str}/
+
+   if [ "generate_weights" -eq "1" ]
+   then
+     cp input.nml.weights input.nml
+   else
+     DIRNAME=${datadir}
+     OUTPUTFILE=grid_fv3.nc
+     WEIGHTFILE=/work2/noaa/gsienkf/weihuang/tools/weiinterp/weights.nc
+     NUM_TYPES=5
+     DATATYPES="'fv_core.res.tile', 'sfc_data.tile', 'fv_tracer.res.tile', 'fv_srf_wnd.res.tile', 'phy_data.tile',"
+
+     sed -e "s?DIRNAME?${DIRNAME}?g" \
+         -e "s?OUTPUTFILE?${OUTPUTFILE}?g" \
+         -e "s?WEIGHTFILE?${WEIGHTFILE}?g" \
+         -e "s?NUM_TYPES?${NUM_TYPES}?g" \
+         -e "s?DATATYPES?${DATATYPES}?g" \
+         input.nml.template > input.nml
+   fi
+
+   ${executable}
+
+   if [ $n -lt 10 ]
+   then
+     ensfile=ens1_00000${n}.nc
+   elif [ $n -lt 100 ]
+   then
+     ensfile=ens1_0000${n}.nc
+   else
+     ensfile=ens1_000${n}.nc
+   fi
+
+   mv grid_fv3.nc ${latlondir}/${ensfile}
+
+   n=$(( $n + 1 ))
+ done
+
+#nemsrc=/work/noaa/gsienkf/weihuang/jedi/vis_tools/sergey.samples/RESTART/
+
+#python ocean2latlon.py --nemsrc=${nemsrc}
+
+#icesrc=/work/noaa/gsienkf/weihuang/jedi/vis_tools/sergey.samples/RESTART/
+
+#python ice2latlon.py --icesrc=${icesrc}
+
