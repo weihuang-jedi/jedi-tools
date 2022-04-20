c66cf56efac2ac971fb770d92b8378f6931f3864
Wei Huang <wei.huang@noaa.gov>
2022-04-20 11:03:15 -0500

    working on

--- python-genobs/writeIODA2Obs.py
+++ python-genobs/writeIODA2Obs.py
@@ -7,13 +7,49 @@ import numpy as np
 import netCDF4
 
 from benedict import benedict
-#import benedict
+
+#=========================================================================
+#benedict is well tested and documented, check the README to see all the features:
+#https://github.com/fabiocaccamo/python-benedict
+class ReadYAML():
+  def __init__(self, debug=0, yamlpath=None):
+    self.debug = debug
+    self.yamlpath = yamlpath
+    self.set_yamlpath(yamlpath=yamlpath)
+
+    if(self.debug):
+      print('debug = ', debug)
+      print('yamlpath = ', yamlpath)
+      self.print()
+
+  def set_yamlpath(self, yamlpath=None):
+   #path can be a yaml string, a filepath or a remote url
+    self.yamlpath = yamlpath
+
+    self.dict = benedict.from_yaml(self.yamlpath)
+
+   #print(self.dict)
+
+  def get_dict(self):
+    return self.dict
+
+  def print(self):
+    n = 0
+    for key in self.dict.keys():
+      n += 1
+      val = self.dict[key]
+      print('key %d: %s' %(n, key))
+      print('\tval:', val)
+
+   #write it back to disk
+   #dict.to_yaml(filepath=self.yamlpath)
 
 #=========================================================================
 class WriteIODA2Obs():
-  def __init__(self, debug=0, filename=None):
+  def __init__(self, debug=0, filename=None, yamlfile=None):
     self.debug = debug
     self.filename = filename
+    self.yamlpath = yamlfile
 
     if(self.debug):
       print('debug = ', debug)
@@ -24,25 +60,30 @@ class WriteIODA2Obs():
     self.nstring = 0
     self.ngrps = 0
 
+    ry = ReadYAML(yamlpath=yamlfile)
+    self.dict = ry.get_dict()
+
     self.set_filename(self, filename=filename)
 
   def set_filename(self, filename=None):
     self.filename = filename
     self.createNCfile()
+    self.closeNCfile()
 
   def closeNCfile(self):
     self.ncfile.close()
 
   def createNCfile(self):
-    self.ncfile = netCDF4.Dataset('./ioda_v2_sondes_sample.nc', 'w', format='NETCDF4')
+    self.ncfile = netCDF4.Dataset(self.filename, 'w', format='NETCDF4')
 
     self.ncfile.title='Manually created IODA V2 Sondes data'
     self.ncfile._ioda_layout_version = 0
-    print(self.ncfile.title)
-    print(self.ncfile)
-    print(self.ncfile._ioda_layout_version)
 
-    self.date_time = netCDF4.stringtochar(np.array(['2020-01-10T06:58:21Z'], 'S20'))
+    print(self.ncfile.title)
+    print(self.ncfile._ioda_layout_version)
+    print(self.ncfile)
+
+   #self.date_time = netCDF4.stringtochar(np.array(['2020-01-10T06:58:21Z'], 'S20'))
 
    #Create the unlimited time dimension:
     self.ncfile.createDimension('nlocs', None)
@@ -73,10 +114,7 @@ class WriteIODA2Obs():
     return var
 
   def put_grpvar(self, grpname, varname, var, val1, val2):
-    ncfile = netCDF4.Dataset(self.filename, 'r+')
-
     if (grpname is None):
-     #var = ncfile.variables[varname][:]
       I = np.where(var == val1)
       var[I] = val2
       ncfile.variables[varname][:] = var
@@ -112,47 +150,20 @@ class WriteIODA2Obs():
 
     return lat, lon
 
-
-#=========================================================================
-#benedict is well tested and documented, check the README to see all the features:
-#https://github.com/fabiocaccamo/python-benedict
-class ReadYAML():
-  def __init__(self, debug=0, yamlpath=None):
-    self.debug = debug
-    self.yamlpath = yamlpath
-
-    if(self.debug):
-      print('debug = ', debug)
-      print('yamlpath = ', yamlpath)
-
-    self.ndatetime = 0
-    self.nlocs = 0
-    self.nstring = 0
-    self.ngrps = 0
-
-    self.set_yamlpath(yamlpath=yamlpath)
-
-  def set_yamlpath(self, yamlpath=None):
-   #path can be a yaml string, a filepath or a remote url
-    self.yamlpath = yamlpath
-
-    dict = benedict.from_yaml(self.yamlpath)
-
-    print(dict)
-
-   #write it back to disk
-   #dict.to_yaml(filepath=self.yamlpath)
-
 #-----------------------------------------------------------------------------------------
 if __name__ == '__main__':
   debug = 1
-  yamlfile = 'var.yaml'
+  filename = './ioda_v2_sondes_sample.nc'
+  yamlfile = 'base.yaml'
+  datafile = 'data.yaml'
 
   opts, args = getopt.getopt(sys.argv[1:], '', ['debug=', 'yamlfile=', 'datafile='])
 
   for o, a in opts:
     if o in ('--debug'):
       debug = int(a)
+    elif o in ('--filename'):
+      filename = a
     elif o in ('--yamlfile'):
       yamlfile = a
     elif o in ('--datafile'):
@@ -160,5 +171,5 @@ if __name__ == '__main__':
    #else:
    #  assert False, 'unhandled option'
 
-  ry = ReadYAML(yamlpath=yamlfile)
+  wio = WriteIODA2Obs(filename=filename, yamlfile=yamlfile)
 
