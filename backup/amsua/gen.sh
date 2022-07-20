35e57200e6f7d197551a5f54f39d129dfff03fe7
Wei Huang <wei.huang@noaa.gov>
2022-07-20 11:12:31 -0500

    add backup

---
+++ backup/amsua/gen.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+ set -x
+
+ topdir=/work2/noaa/gsienkf/weihuang/jedi/case_study/amsua
+
+ taskspernode=40
+ NUMMEM=80
+ MYLAYOUT=1,20
+
+#cpu_list=(   6    12    18    24    30    36     72     78)
+#nodelist=(   1     1     1     1     1     1      2      2)
+#lay_list=("1,1" "1,2" "1,3" "1,4" "1,5" "1,6" "1,12" "1,13")
+
+#cpu_list=(  24)
+#nodelist=(   1)
+#lay_list=("2,2")
+
+ cpu_list=(  36)
+ nodelist=(   1)
+ lay_list=("2,3")
+
+#-----------------------------------------------------------------------------
+ cd ${topdir}
+ ln -sf ../Data .
+#ln -sf ../ioda_v2_data .
+
+ for i in ${!cpu_list[@]}
+ do
+   echo "element $i is ${myArray[$i]}"
+   totalcpus=${cpu_list[$i]}
+   nodes=${nodelist[$i]}
+   MYLAYOUT=${lay_list[$i]}
+
+   workdir=${topdir}/run_${NUMMEM}.${taskspernode}t${nodes}n_${totalcpus}p
+   mkdir -p ${workdir}
+   cd ${workdir}
+
+   sed -e "s?TASKSPERNODE?${taskspernode}?g" \
+       -e "s?TOTALNODES?${nodes}?g" \
+       -e "s?TOTALCPUS?${totalcpus}?g" \
+       -e "s?WORKDIR?${workdir}?g" \
+       -e "s?NUMMEM?${NUMMEM}?g" \
+       -e "s?MYLAYOUT?${MYLAYOUT}?g" \
+       ${topdir}/template.slurm > run.slurm
+
+   sed -e "s?LAYOUT?${MYLAYOUT}?" \
+       -e "s?NUMBEROFMEMBERS?${NUMMEM}?" \
+       ${topdir}/getkf.yaml.template > getkf.yaml
+
+   if [ $i -lt 1 ]
+   then
+    #DEPEND=$(sbatch --parsable run.slurm)
+    #echo "job_id: ${DEPEND}"
+     sbatch run.slurm
+   else
+    #DEPEND=$(sbatch --dependency=afterany:${DEPEND} --parsable run.slurm)
+     sbatch run.slurm
+   fi
+
+   cd ${topdir}
+ done
+
---
+++ backup/diag2iodav2/t.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ set -x
+
+#ioda-bundle build dir:
+ export ioda_dir=/work2/noaa/gsienkf/weihuang/jedi/ioda-bundle
+
+#output dir.
+ output_dir=/work2/noaa/gsienkf/weihuang/jedi/case_study/diag2iodav2/out
+
+#input file fir.
+ input_dir=/work2/noaa/gsienkf/weihuang/jedi/case_study/diag2iodav2/obs
+ 
+ export PYTHONPATH=/work2/noaa/gsienkf/weihuang/jedi/ioda-bundle/build/lib/python3.8/pyioda:$PYTHONPATH
+#Convert to ioda_v1 data
+ python ${ioda_dir}/build/bin/proc_gsi_ncdiag.py -o ${output_dir} ${input_dir}
+
+ exit 0
+
+#file name of the combined file.
+ flnm=ps_obs_2020011006.nc4
+
+#Combine the files to a single ioda-v1 obs. file
+ python ${ioda_dir}/iodaconv/src/gsi-ncdiag/combine_files.py \
+ 	-i out/sfc_ps_obs_2020011006.nc4 \
+ 	 out/sfcship_ps_obs_2020011006.nc4 \
+ 	 out/sondes_ps_obs_2020011006.nc4 \
+ 	-o $flnm
+
+#ncrename -h -O -v datetime@MetaData,LaunchTime@MetaData ${flnm}
+
+#Convert from ioda-v1 to ioda-v2.
+ /work/noaa/gsienkf/weihuang/jedi/src/my.compile/bin/ioda-upgrade.x $flnm ioda_v2_${flnm}
+
